89cff12fbe8db009ef5548423e45999288e7f8cc
DeibyRayo <deiby.developer@gmail.com>
2023-01-25 22:08:15 -0500

    default-params

---
+++ src/es6/02-strings.js
@@ -0,0 +1,19 @@
+// Template Literals
+
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+console.log(epicPhrase);
+
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2)
+
+// Multi-line strings
+
+let lorem = "esto es un string \n " + "esto es otra linea";
+
+let lorem2 = `esta es un frase epica
+la continuación de una frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
