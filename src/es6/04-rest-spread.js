3bac69b1d8947cefb9d4fdb2ffa77dda618d1f72
DeibyRayo <deiby.developer@gmail.com>
2023-01-26 06:02:19 -0500

    spread operator and rest

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -6,6 +6,24 @@ console.log(a, fruit[1]);
 
 //Object destructuring
 
-let user = {username: 'Deiby', age: 19};
-let {username, age} = user;
+let user = { username: "Deiby", age: 19 };
+let { username, age } = user;
 console.log(username, user.age);
+
+// spread operator
+
+let person = { name: "Deiby", age: 19 };
+let country = "CO";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+// rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
