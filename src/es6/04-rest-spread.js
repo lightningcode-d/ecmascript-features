0beffcf7e1fbfaceaf2ac47f51397c3dae2b1825
DeibyRayo <deiby.developer@gmail.com>
2023-01-26 05:53:10 -0500

    destructuring

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+// aarays destructuring
+
+let fruit = ["Apple", "Banana"];
+let [a, b] = fruit;
+console.log(a, fruit[1]);
+
+//Object destructuring
+
+let user = {username: 'Deiby', age: 19};
+let {username, age} = user;
+console.log(username, user.age);
