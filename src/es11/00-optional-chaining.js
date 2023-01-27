72b7756e4cb10607dd0f53d3f6419fd86bd3e29d
DeibyRayo <deiby.developer@gmail.com>
2023-01-27 07:41:11 -0500

    es11: optional-chaning

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,11 @@
+const users = {
+    lightning: {
+        country: "CO"
+    },
+    seb: {
+        country: "CO"
+    }
+}
+
+//console.log(users.lightning.country);
+console.log(users?.bebeloper?.country);
